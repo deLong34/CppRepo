3dd0689f5bc2dc01f1a44f46ef84483a5de81789
deLong34 <96695069+deLong34@users.noreply.github.com>
2022-02-28 04:10:34 +0300

    Delete mylib.h

--- mylib.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace myLib {
-	void initArray(float array[], int size);  //прототип функции инициализации массива
-	void printArray(float array[], int size); //прототип функции вывода на экран массива
-	void myCalc(float array[], int size);     //прототип функции подсчета + и - элементов
-}
